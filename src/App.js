8f005ec9e76d52fdf5aa8d688868af7a7fa5fcd7
Doom Dude <doom@doomdude.com>
2022-08-06 22:19:28 +0300

    adding a new component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Footer } from "./components/common/footer/Footer.js";
 import { Header } from "./components/common/header/Header.js";
 import { Cursor } from "./components/common/cursor/Cursor.js";
 import { HomePage } from "./components/home-page/HomePage.js";
+import { RandomCocktails } from "./components/random-cocktails-page/RandomCocktails.js";
 // import { AgeGateModal } from "./components/common/age-gate/AgeGateModal.js";
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Header />
       {/* <AgeGateModal /> */}
       <HomePage />
+      <RandomCocktails />
       <Footer />
       <Cursor />
     </div>
